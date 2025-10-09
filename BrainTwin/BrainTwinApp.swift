636dc8c26e6307c14f6adf3f421791b2763dfcfc
Dastan <asymbaevdastan@gmail.com>
2025-10-08 23:38:18 -0400

    Initial Commit

---
+++ BrainTwin/BrainTwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTwinApp.swift
+//  BrainTwin
+//
+//  Created by Dastan Asymbaev on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BrainTwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
